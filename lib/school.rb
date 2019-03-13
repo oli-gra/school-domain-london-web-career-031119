e2dd63c20acb06bfd0d72cdd49ea9ae050abf070
Oliver <oliver.grant@gmail.com>
2019-03-13 14:45:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+
+class School
+  
+  attr_accessor :student, :grade
+  
+  @roster = {}
+  @roster[grade] = []
+  
+  def add_student(name,grade)
+    @name = name
+    @grade = grade
+    @roster[grade] << @name
+  end
+  
+end
