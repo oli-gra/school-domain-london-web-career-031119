e823a09ba3a409fbb0c2eb9c7d86a0cec69d6f49
Oliver <oliver.grant@gmail.com>
2019-03-13 15:22:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require'pry'
+
+class School
+  
+  attr_accessor :student, :roster
+  attr_writer :grade
+  
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def grade(search)
+    @roster.select {|grade,students| search == grade}.values.flatten
+  end
+  
+  def sort
+    sorted = {}
+    @roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end    
+
+  def add_student(name,grade)
+    @name = name
+    @grade = grade
+    if @roster.key?(grade)
+      @roster[grade] << @name
+    else
+      @roster[grade] = []
+      @roster[grade] << @name
+    end
+  end
+  
+end
