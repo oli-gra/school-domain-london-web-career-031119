8ab04cd0a5a36db113d934f94963b81efa34e247
Oliver <oliver.grant@gmail.com>
2019-03-13 15:06:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,7 @@ class School
   
   def sort
     new_hash = {}
-    @roster.min_by{|grade,students| grade}
+    new_hash = @roster.min_by{|grade,students| grade}
     binding.pry
     puts"yeh"
   end
